673503ec97e5f2686634791f1bf5f22f9ff323b0
Carolyn Manning <carolyn.manning3@gmail.com>
2020-10-13 01:30:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -32,7 +32,6 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts "#{season.to_s.capitalize!}:"
     holidays.each do |holiday, supplies|
-      binding.pry
       string_holiday = holiday.to_s.split("_").each {|day| day.capitalize!}.join(" ")
       puts "  #{string_holiday}: #{supplies.join(", ")}"
     end
