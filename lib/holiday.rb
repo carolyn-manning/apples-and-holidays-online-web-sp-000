41560b90b4a28932689aa2d8aabde8d1005ae86f
Carolyn Manning <carolyn.manning3@gmail.com>
2020-10-13 01:45:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -47,12 +47,14 @@ end
 
 
 def all_holidays_with_bbq(holiday_hash)
+  holiday_array = []
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday, supplies|
       supplies.include?("BBQ")
-        holiday
+        holiday_array << holiday
     end
   end
+  holiday_array
 end
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
