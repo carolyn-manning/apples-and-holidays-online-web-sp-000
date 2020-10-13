f5d96ad41f71109db5da94b7b2b41703a2f76f10
Carolyn Manning <carolyn.manning3@gmail.com>
2020-10-13 01:35:49 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,8 +56,6 @@ def all_holidays_with_bbq(holiday_hash)
       end
     end
   end
-end 
+end
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
-
